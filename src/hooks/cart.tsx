7acff602be9c153d171fd960b1157eef39a8c8c1
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-08-20 13:06:00 -0300

    Increment and decrement functions

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -43,6 +43,7 @@ const CartProvider: React.FC = ({ children }) => {
   const addToCart = useCallback(
     async product => {
       const productExists = products.find(prod => prod.id === product.id);
+
       if (productExists) {
         setProducts(
           products.map(p =>
@@ -50,19 +51,45 @@ const CartProvider: React.FC = ({ children }) => {
           ),
         );
       } else {
-        setProducts([...products, { ...product, quantity: 1 }]);
+        const newProduct = { ...product, quantity: 1 };
+        setProducts([...products, newProduct]);
+
+        await AsyncStorage.setItem(
+          '@market:products',
+          JSON.stringify(newProduct),
+        );
       }
     },
     [products],
   );
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      products.map(p =>
+        p.id === id ? { ...products, quantity: p.quantity + 1 } : p,
+      );
+    },
+    [products],
+  );
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const decrement = useCallback(
+    async id => {
+      const product = products.find(p => p.id === id);
+      if (!product) {
+        throw new Error('eita');
+      }
+      if (product.quantity === 1) {
+        const productRemoved = products.filter(p => p.id !== id);
+
+        setProducts(productRemoved);
+      } else {
+        products.map(p =>
+          p.id === id ? { ...product, quantity: p.quantity - 1 } : p,
+        );
+      }
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
